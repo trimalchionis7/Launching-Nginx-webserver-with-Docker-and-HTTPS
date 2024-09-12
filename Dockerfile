0b22a5492ef5853816329f9ac99dfca8dd2ef6c0
Jonathan Griffiths <je.griffiths@live.co.uk>
2024-09-12 18:41:14 +0100

    Syncing with remote

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# Use Nginx as base image
-FROM nginx:latest
-
-# Use CGI greeting as index page
-COPY index.html /usr/share/nginx/html/index.html
-
-# Expose port 80
-EXPOSE 80
---
+++ vm-infrastructure/variables.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  description = "Key pair resource for EC2"
+  default     = "jonnie-vpc"
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "eu-central-1"
+}
